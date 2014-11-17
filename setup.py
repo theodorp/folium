415c508b8198446f2d0fc6140847321b373a95bf
Theodor Perivolaris <theodor.perivolaris@gmail.com>
2014-11-17 01:44:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ pkg_data = {
 
 setup(
     name='folium',
-    version='0.1.2',
+    version='0.1.3',
     description='Make beautiful maps with Leaflet.js & Python',
     author='Rob Story',
     author_email='wrobstory@gmail.com',
